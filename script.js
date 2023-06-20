c4c0b737e448ca5090d550af380895916d9aa6bf
S1S3 <136255986+PH-S1S3@users.noreply.github.com>
2023-06-20 09:24:14 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// welcome
+alert('Welcome to PH-S1S3 website');
+
+let firstName = 'https://ph-s1s3.github.io/secret_web.html';
+
+console.log(firstName);
+
+/*
+// Password
+var password = "ph-s1s3";
+(function passcodeprotect() {
+    var passcode = prompt("Enter PassCode to Enter the website");
+    while (passcode !== password) {
+        alert("Incorrect PassCode");
+        return passcodeprotect();
+    }
+}());
+alert('Correct Password');
+*/
