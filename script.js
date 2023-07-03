b89eb4100b282d8424e6c6925acb36478cfd7331
S1S3 <136255986+PH-S1S3@users.noreply.github.com>
2023-07-03 21:19:09 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+// welcome
+alert('Welcome to PH-S1S3 website');
+
+const secretWebLink = 'https://ph-s1s3.github.io/secret_web.html';
+let thisYear = 2023;
+const ageClassS1S3 = thisYear - 2007;
+const textOfAge = '今年我们';
+const textAge = '岁';
+
+console.log(secretWebLink);
+console.log(textOfAge + ageClassS1S3 + textAge);
+
+/*
+var password = "ph-s1s3";
+(function passcodeprotect() {
+    var passcode = prompt("Enter PassCode to Enter the website");
+    while (passcode !== password) {
+        alert("Incorrect PassCode");
+        return passcodeprotect();
+    }
+}());
+alert('Correct Password');
+*/
+
+
+
+
+
+/*
+var password = "ph-s1s3";
+if (once_per_session == 0)
+    passcodeprotect();
+else
+    loadornot();
+*/
+
+
+
+/*
+//var password = "ph-s1s3";
+function passcodeprotect() {
+var passcode = prompt("Enter PassCode to Enter the website");
+while (passcode !== password) {
+    alert("Incorrect PassCode");
+    return passcodeprotect();
+}
+alert('Correct Password');
+};
+*/
+
+
+
+
+/*
+// Password
+var password = "ph-s1s3";
+(function passcodeprotect() {
+    var passcode = prompt("Enter PassCode to Enter the website");
+    while (passcode !== password) {
+        alert("Incorrect PassCode");
+        return passcodeprotect();
+    }
+    alert('Correct Password');
+}());
+*/
+
+
---
+++ script_secret.js
@@ -0,0 +1,17 @@
+// welcome
+let secretWebFirstName = 'Welcome to the secret website of PH-S1S3';
+
+alert(secretWebFirstName);
+
+/*
+// Password
+var password = "ph-s1s3";
+(function passcodeprotect() {
+    var passcode = prompt("Enter PassCode to Enter the website");
+    while (passcode !== password) {
+        alert("Incorrect PassCode");
+        return passcodeprotect();
+    }
+}());
+alert('Correct Password');
+*/
