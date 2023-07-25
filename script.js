d182d333a0dc1da73d26eb8718cce263773b3e98
S1S3 <136255986+PH-S1S3@users.noreply.github.com>
2023-07-25 10:01:38 +0800

    Update script.js
    added Gaming web

--- script.js
+++ script.js
@@ -2,13 +2,18 @@
 alert('Welcome to PH-S1S3 website');
 
 const secretWebLink = 'https://ph-s1s3.github.io/secret_web.html';
+const gamingLink = "https://ph-s1s3.github.io/Game.html"
 let thisYear = 2023;
 const ageClassS1S3 = thisYear - 2007;
 const textOfAge = '今年我们';
 const textAge = '岁';
 
+
 console.log(secretWebLink);
+console.log(gamingLink)
 console.log(textOfAge + ageClassS1S3 + textAge);
+
+
 
 /*
 var password = "ph-s1s3";
